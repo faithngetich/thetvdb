b355a5f0c3da41afa5ffcfd15345bd5a1bcd9fe7
faithngetich <faith.ngetich@andela.com>
2018-04-16 11:20:12 +0300

    initial commit

---
+++ lib/generators/templates/thetvdb.rb
@@ -0,0 +1,2 @@
+require 'thetvdb'
+API_KEY = "9EF1D1E7D28FDA0B"
---
+++ spec/thetvdb_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Thetvdb do
+  it "has a version number" do
+    expect(Thetvdb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
